af8a261fe6ff0dca7447e0135c3fbfc7ed6be392
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-11 22:07:31 -0300

    router auth

---
+++ router/auth.js
@@ -0,0 +1,17 @@
+/*
+
+*/
+
+const { Router } = require('express');
+const router = Router();
+
+router.post('/new',(req, res)=>{
+    res.json({
+        ok:true,
+    })
+});
+
+
+
+
+module.exports = router;
