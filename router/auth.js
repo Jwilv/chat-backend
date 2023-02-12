1a1d1640136a6c27690b9624f488e558cf9a258e
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-11 22:15:11 -0300

    router login, renew

--- router/auth.js
+++ router/auth.js
@@ -5,9 +5,27 @@
 const { Router } = require('express');
 const router = Router();
 
+//nuevo user, Register
 router.post('/new',(req, res)=>{
     res.json({
         ok:true,
+        msg:'register',
+    })
+});
+
+//logeo de user ya existente, Login
+router.post('/',(req, res)=>{
+    res.json({
+        ok:true,
+        msg:'Login',
+    })
+});
+
+//renovar token, Renew
+router.get('/renew',(req, res)=>{
+    res.json({
+        ok:true,
+        msg:'renew',
     })
 });
 
