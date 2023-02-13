6d6ef1a13e23491beda68ecbae2045de8a8b2965
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-12 21:20:49 -0300

    se agrego getMessageChat a router

--- router/message.js
+++ router/message.js
@@ -4,11 +4,12 @@ host + /api/message
 */
 
 const { Router } = require('express');
+const { getMessageChat } = require('../controllers/message');
 const { validateToken } = require('../middlewares/validate-token');
 const router = Router();
 
 
-router.get('/:de', validateToken)
+router.get('/:de', validateToken, getMessageChat)
 
 
 
