144b1da02720e4dec36a13b354adabbcd17bf845
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-20 18:25:08 -0300

    disconnect

--- models/sockets.js
+++ models/sockets.js
@@ -13,7 +13,11 @@ class Sockets {
     socketEvents() {
         // on connection
         this.io.on('connection', (socket) => {
-        
+        console.log('user connect');
+
+        socket.on('disconnect',()=>{
+            console.log('user disconnect');
+        })
 
         })
 
