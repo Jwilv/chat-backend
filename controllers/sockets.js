b8b0a693d016d2fe2d5cda10da7d0a8540e5c83e
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-20 22:00:25 -0300

    userDisconnect DB

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -12,6 +12,19 @@ const userConnect = async(uid)=>{
 
 }
 
+const userDisconnect = async(uid)=>{
+
+    const user = await User.findById(uid);
+
+    user.online = false;
+
+    await user.save();
+
+    return user
+
+}
+
 module.exports={
-    userConnect
+    userConnect,
+    userDisconnect,
 }
